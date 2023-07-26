bbbfbb41106a0fdd0a5d1b06ec8ee3188ff85129
Loic Leblé <leble.loic@gmail.com>
2023-07-26 17:01:24 +0200

    antoine made optimization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,43 +12,43 @@ const db = admin.firestore();
 // Firebase Function déclenchée lors de l'upload d'une image
 export const processImage = functions.storage.object().onFinalize(
   async (object) => {
-    const collectionName = "Analysis";
-
-    const fileBucket = object.bucket;
-    const filePath = object.name? object.name : "undefined";
-
-    interface Score {
-      tag : string,
-      score : number
-    }
-
-    const entry = db.collection(collectionName).doc(filePath);
-    entry.set({
-      isAnalysed: false,
-    });
-
-    const scores : Score[] = [];
-
-    functions.logger.error("************* MAMAN **********");
-
-    // Vérifiez si l'objet est une image
-    if (!filePath || (!filePath.endsWith(".jpg") &&
-  !filePath.endsWith(".jpeg") &&
-  !filePath.endsWith(".png"))) {
-      functions.logger.error("filePath est null.");
-      return null;
-    }
-
     try {
-    // Appel de l'API Web Detection
+      const collectionName = "Analysis";
+
+      const fileBucket = object.bucket;
+      const filePath = object.name ? object.name : "undefined";
+
+      interface Score {
+        tag: string,
+        score: number
+      }
+
+      const entry = db.collection(collectionName).doc(filePath);
+      entry.set({
+        isAnalysed: false,
+      });
+
+      const scores: Score[] = [];
+
+      functions.logger.error("************* MAMAN **********");
+
+      // Vérifiez si l'objet est une image
+      if (!filePath || (!filePath.endsWith(".jpg") &&
+        !filePath.endsWith(".jpeg") &&
+        !filePath.endsWith(".png"))) {
+        functions.logger.error("filePath est null.");
+        return null;
+      }
+
+      // Appel de l'API Web Detection
       const [result] = await client.webDetection(`gs://${fileBucket}/${filePath}`);
       const webDetection = result?.webDetection;
 
       // Vérifiez si des résultats de détection sont disponibles
       if (webDetection) {
-      // Traitez les résultats
+        // Traitez les résultats
         if (webDetection.fullMatchingImages &&
-            webDetection.fullMatchingImages.length > 0) {
+          webDetection.fullMatchingImages.length > 0) {
           functions.logger.info(`*** Web detection for ${filePath} ***`);
           const entities = webDetection.webEntities;
 
@@ -71,9 +71,13 @@ export const processImage = functions.storage.object().onFinalize(
 
       entry.set({
         tags: scores,
-        isAnalysed: true});
+        isAnalysed: true
+      });
+
+      return null;
     } catch (error) {
       functions.logger.error("Web Detection Error:", error);
+      return null;
     }
 
     return null;
